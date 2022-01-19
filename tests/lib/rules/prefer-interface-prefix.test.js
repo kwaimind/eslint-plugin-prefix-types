e5cf84d0c2f22cf2501ccab6473af927b926e181
Daniel Reed <danieel.reed@gmail.com>
2022-01-19 13:07:56 +0100

    using eslint options instead of 2 tests

--- tests/lib/rules/prefer-interface-prefix.test.js
+++ tests/lib/rules/prefer-interface-prefix.test.js
@@ -10,33 +10,94 @@ RuleTester.setDefaultConfig(options);
 
 const tester = new RuleTester();
 
-tester.run(`rule: ${rule.meta.name}`, rule, {
+tester.run(`${rule.meta.name}: allow: "always"`, rule, {
   valid: [
-    "interface IAnotherInterface { preview: boolean; }",
-    "interface IInfoBoxProps { preview: boolean; }",
-    "interface ICustomProps extends AppProps { preview: boolean; }",
-    "export interface ICustomProps { preview: boolean; }",
+    {
+      code: "interface IAnotherInterface { preview: boolean; }",
+      options: [{ allow: "always" }],
+    },
+    {
+      code: "interface IInfoBoxProps { preview: boolean; }",
+      options: [{ allow: "always" }],
+    },
+    {
+      code: "interface ICustomProps extends AppProps { preview: boolean; }",
+      options: [{ allow: "always" }],
+    },
+    {
+      code: "export interface ICustomProps { preview: boolean; }",
+      options: [{ allow: "always" }],
+    },
   ],
   invalid: [
     {
       code: "interface AnotherInterface { preview: boolean; }",
       errors: [{ message: "Interfaces must start with a capital I" }],
       output: "interface IAnotherInterface { preview: boolean; }",
+      options: [{ allow: "always" }],
     },
     {
       code: "interface CustomProps extends AppProps { preview: boolean; }",
       errors: [{ message: "Interfaces must start with a capital I" }],
       output: "interface ICustomProps extends AppProps { preview: boolean; }",
+      options: [{ allow: "always" }],
     },
     {
       code: "interface InfoBoxProps { preview: boolean; }",
       errors: [{ message: "Interfaces must start with a capital I" }],
       output: "interface IInfoBoxProps { preview: boolean; }",
+      options: [{ allow: "always" }],
     },
     {
       code: "interface myInfoProps { preview: boolean; }",
       errors: [{ message: "Interfaces must start with a capital I" }],
       output: "interface IMyInfoProps { preview: boolean; }",
+      options: [{ allow: "always" }],
+    },
+  ],
+});
+
+tester.run(`${rule.meta.name}: allow: "never"`, rule, {
+  valid: [
+    {
+      code: "interface AnotherInterface { preview: boolean; }",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "interface InfoBoxProps { preview: boolean; }",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "interface CustomProps extends AppProps { preview: boolean; }",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "export interface CustomProps { preview: boolean; }",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "interface myInfoProps { preview: boolean; }",
+      options: [{ allow: "never" }],
+    },
+  ],
+  invalid: [
+    {
+      code: "interface IAnotherInterface { preview: boolean; }",
+      errors: [{ message: "Interfaces must not start with a capital I" }],
+      output: "interface AnotherInterface { preview: boolean; }",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "interface ICustomProps extends AppProps { preview: boolean; }",
+      errors: [{ message: "Interfaces must not start with a capital I" }],
+      output: "interface CustomProps extends AppProps { preview: boolean; }",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "interface IInfoBoxProps { preview: boolean; }",
+      errors: [{ message: "Interfaces must not start with a capital I" }],
+      output: "interface InfoBoxProps { preview: boolean; }",
+      options: [{ allow: "never" }],
     },
   ],
 });
