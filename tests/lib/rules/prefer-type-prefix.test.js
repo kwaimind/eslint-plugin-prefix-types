d7a90dba889630ee5f301bddd00a0f56036c5b62
Daniel Reed <danieel.reed@gmail.com>
2022-01-19 15:52:04 +0100

    adding context options to type prefix rule

--- tests/lib/rules/prefer-type-prefix.test.js
+++ tests/lib/rules/prefer-type-prefix.test.js
@@ -10,38 +10,96 @@ RuleTester.setDefaultConfig(options);
 
 const tester = new RuleTester();
 
-tester.run(`rule: ${rule.meta.name}`, rule, {
+tester.run(`${rule.meta.name}: allow: "always"`, rule, {
   valid: [
-    'type TMyType = "yes" | "no"',
-    'export type TMyType = "yes" | "no"',
-    'type TInfo = "test1" | "test2"',
-    'type TTestType = "test1" | "test2"',
+    { code: 'type TMyType = "yes" | "no"', options: [{ allow: "always" }] },
+    {
+      code: 'export type TMyType = "yes" | "no"',
+      options: [{ allow: "always" }],
+    },
+    { code: 'type TInfo = "test1" | "test2"', options: [{ allow: "always" }] },
+    {
+      code: 'type TTestType = "test1" | "test2"',
+      options: [{ allow: "always" }],
+    },
   ],
   invalid: [
     {
       code: 'type MyType = "yes" | "no"',
       errors: [{ message: "Types must start with a capital T" }],
       output: 'type TMyType = "yes" | "no"',
+      options: [{ allow: "always" }],
     },
     {
       code: 'type TestType = "test1" | "test2"',
       errors: [{ message: "Types must start with a capital T" }],
       output: 'type TTestType = "test1" | "test2"',
+      options: [{ allow: "always" }],
     },
     {
       code: "type InfoType = true | false",
       errors: [{ message: "Types must start with a capital T" }],
       output: "type TInfoType = true | false",
+      options: [{ allow: "always" }],
     },
     {
       code: "type TestType = true | false",
       errors: [{ message: "Types must start with a capital T" }],
       output: "type TTestType = true | false",
+      options: [{ allow: "always" }],
     },
     {
       code: "type myType = true | false",
       errors: [{ message: "Types must start with a capital T" }],
       output: "type TMyType = true | false",
+      options: [{ allow: "always" }],
+    },
+  ],
+});
+
+tester.run(`${rule.meta.name}: allow: "never"`, rule, {
+  valid: [
+    { code: 'type MyType = "yes" | "no"', options: [{ allow: "never" }] },
+    {
+      code: 'export type MyType = "yes" | "no"',
+      options: [{ allow: "never" }],
+    },
+    { code: 'type Info = "test1" | "test2"', options: [{ allow: "never" }] },
+    {
+      code: 'type TestType = "test1" | "test2"',
+      options: [{ allow: "never" }],
+    },
+  ],
+  invalid: [
+    {
+      code: 'type TMyType = "yes" | "no"',
+      errors: [{ message: "Types must not start with a capital T" }],
+      output: 'type MyType = "yes" | "no"',
+      options: [{ allow: "never" }],
+    },
+    {
+      code: 'type TTestType = "test1" | "test2"',
+      errors: [{ message: "Types must not start with a capital T" }],
+      output: 'type TestType = "test1" | "test2"',
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "type TInfoType = true | false",
+      errors: [{ message: "Types must not start with a capital T" }],
+      output: "type InfoType = true | false",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "type TTestType = true | false",
+      errors: [{ message: "Types must not start with a capital T" }],
+      output: "type TestType = true | false",
+      options: [{ allow: "never" }],
+    },
+    {
+      code: "type TMyType = true | false",
+      errors: [{ message: "Types must not start with a capital T" }],
+      output: "type MyType = true | false",
+      options: [{ allow: "never" }],
     },
   ],
 });
